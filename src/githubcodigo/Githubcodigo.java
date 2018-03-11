eda2e5de9c660a0006492a37f3b967d365ec02eb
Kilmer lopez <kilmerlopez3@hotmail.com>
2018-03-11 00:11:53 -0600

    primera version

--- src/githubcodigo/Githubcodigo.java
+++ src/githubcodigo/Githubcodigo.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package githubcodigo;
-
+import java.util.*;
 /**
  *
  * @author kilme
@@ -15,7 +15,9 @@ public class Githubcodigo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        system.out.println("hola mundo");
+            System.out.println("hola mundo");
+            System.out.print(2*2);
+            
         // TODO code application logic here
     }
     
