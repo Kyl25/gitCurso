3b90b59477a19dfde3feb72bcf514c0b70bed895
Kilmer lopez <kilmerlopez3@hotmail.com>
2018-03-11 00:01:20 -0600

    iniciando de java

---
+++ src/githubcodigo/Githubcodigo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubcodigo;
+
+/**
+ *
+ * @author kilme
+ */
+public class Githubcodigo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        system.out.println("hola mundo");
+        // TODO code application logic here
+    }
+    
+}
